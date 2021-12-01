7ee3044d060c3ce7b3cb98a8434914c18cfab789
Ruben Nijhuis <contact@rubennijhuis.com>
2021-12-01 20:43:05 +0100

    small fix

--- 01/main.js
+++ 01/main.js
@@ -4,12 +4,12 @@ fs.readFile('./data.txt', 'utf8', (err, file_data) => {
 	let data;
 	let amount_changes;
 
-	amount_changes = 0;
+	amount_changes = 1; // +1 as the start is counted as an increase
 	data = file_data.split(/\n/);
 	for (let i = 0; i < data.length; i++)
 	{
 		if (data[i + 1] > data[i])
 			amount_changes++;
 	}
-	console.log(amount_changes + 1); // +1 fix because last entry isn't counted
+	console.log(amount_changes);
 });
